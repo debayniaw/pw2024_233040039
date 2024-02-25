bd731eeeccba56a4c7635653b58585c2d451e418
debayniaw <nadhiliawww@gmail.com>
2024-02-25 08:32:51 +0000

    first commit

---
+++ tugas/tugas1/tugas1a.php
@@ -0,0 +1,6 @@
+<?php
+$n1 = "Topi";
+$n2 = "Bundar";
+
+echo "<h2><em>\"$n1 saya $n2, $n2 $n1 saya.\"</em></h2>";
+?>
---
+++ tugas/tugas1/tugas1b.php
@@ -0,0 +1,9 @@
+<?php
+$npm = 39;
+
+echo "aku adalah angka <b>$npm</b> <br>";
+echo "Jika aku dikali 5, maka aku sekarang menjadi <b>" . ($npm *=5) . "</b> <br>";
+echo "Jika aku dibagi 2, maka aku sekarang menjadi <b>" . ($npm /=2) . "</b> <br>";
+echo "jika aku ditambah 75, maka aku sekarang menjadi <b>" . ($npm +=75) . "</br> <br>";
+echo "jika aku dikurang 20, maka aku sekarang menjadi <b>" . ($npm -=20) . "</br> <br>";
+?>
